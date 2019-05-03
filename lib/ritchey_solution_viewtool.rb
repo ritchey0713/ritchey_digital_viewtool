376bc3fe6fc612786212c76918e1bb2efe8d45cc
Dj <ritchey0713@gmail.com>
2019-05-03 15:26:58 -0400

    Initial commit

---
+++ lib/ritchey_solution_viewtool.rb
@@ -0,0 +1,6 @@
+require "ritchey_solution_viewtool/version"
+
+module RitcheySolutionViewtool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/ritchey_solution_viewtool/version.rb
@@ -0,0 +1,3 @@
+module RitcheySolutionViewtool
+  VERSION = "0.1.0"
+end
